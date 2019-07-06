08565ab1620af27e18090b34d7f01567e58463f3
kziebinski <ziebinski.konrad@gmail.com>
2019-07-06 15:11:27 +0200

    Add browserSetup.java

--- src/main/java/test/BasicSetupTest.java
+++ src/main/java/test/BasicSetupTest.java
@@ -3,9 +3,6 @@ package test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -17,31 +14,14 @@ public class BasicSetupTest {
 
     protected WebDriver driver;
     protected HomePage homePage;
+    private BrowserSetup browserSetup = new BrowserSetup();
     private final String baseUrl = "http://automationpractice.com/index.php";
     private final Logger log = LogManager.getLogger(BasicSetupTest.class.getName());
 
     @Parameters("browser")
     @BeforeMethod()
     public void beforeRun(String browser) throws Exception {
-        //chrome
-        if (browser.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/chromedriver.exe");
-            driver = new ChromeDriver();
-        }
-        //firefox
-        else if (browser.equalsIgnoreCase("firefox")){
-            System.setProperty("webdriver.gecko.driver", "src/main/resources/webdriver/geckodriver.exe");
-            driver = new FirefoxDriver();
-        }
-        //edge
-        else if (browser.equalsIgnoreCase("edge")){
-            System.setProperty("webdriver.edge.driver","src/main/resources/webdriver/MicrosoftWebDriver.exe");
-            driver = new EdgeDriver();
-        }
-        //If no browser
-        else {
-            throw new Exception("Browser is not correct");
-        }
+        driver = browserSetup.setup(browser);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(baseUrl);
---
+++ src/main/java/test/BrowserSetup.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserSetup {
+    private WebDriver driver;
+
+    public WebDriver setup(String browser) throws Exception {
+        //chrome
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        //firefox
+        else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/webdriver/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        //edge
+        else if (browser.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", "src/main/resources/webdriver/MicrosoftWebDriver.exe");
+            driver = new EdgeDriver();
+        }
+        //If no browser
+        else {
+            throw new Exception("Browser is not correct");
+        }
+        return driver;
+    }
+}
