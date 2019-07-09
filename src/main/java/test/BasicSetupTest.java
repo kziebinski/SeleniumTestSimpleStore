c9ebb53da7b0c2d5b6978854050828a6b65f7a7a
konrad.ziebinski <konrad.ziebinski@globallogic.com>
2019-07-09 14:43:10 +0200

    Add html reporter

--- src/main/java/test/BasicSetupTest.java
+++ src/main/java/test/BasicSetupTest.java
@@ -3,6 +3,7 @@ package test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -28,6 +29,7 @@ public class BasicSetupTest {
         driver.get(baseUrl);
         homePage = new HomePage(driver);
         log.info("Setup complete -> Start testing...");
+        Reporter.log("Setup complete -> Start testing...");
     }
 
     @AfterMethod
